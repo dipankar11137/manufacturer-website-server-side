cca7e812da0422d7c1aa4b31cfda18cabdb22bd2
Dipankar <dipankar15-11137@diu.edu.bd>
2022-05-26 14:48:13 +0600

    simple setup

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+
+
+app.get('/', (req, res) => {
+    res.send('manufacture tools ')
+})
+
+app.listen(port, () => {
+    console.log(`Manufacture tools  port on :  ${port}`)
+})
