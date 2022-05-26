aca2091898640336fa11d23715c59b629afe6d88
Dipankar <dipankar15-11137@diu.edu.bd>
2022-05-26 17:25:50 +0600

    data base connected

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -11,8 +11,28 @@ app.use(express.json());
 
 
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.oqrnv.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+    const collection = client.db("test").collection("devices");
+    // perform actions on the collection object
+    client.close();
+});
+
+async function run() {
+    try {
+        await client.connect();
+        console.log('Database connect')
+    }
+    finally {
+
+    }
+}
+
+run().catch(console.dir);
+
 app.get('/', (req, res) => {
-    res.send('manufacture tools ')
+    res.send('Manufacture tools Running')
 })
 
 app.listen(port, () => {
